9052aa74f1e010f81dfea86ef75260a7a49e97df
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-03-29 17:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 var Promise = new Promise(function(resolve,reject){
-	setTimeOut(resolve("Hello, world!"),1000);
+	return setTimeOut(resolve("Hello, world!"),1000);
 });
 Promise.then((msg)=>{
 	document.querySelector("#output").innerHTML = msg;
